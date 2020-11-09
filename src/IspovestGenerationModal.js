bdbd6c5fe957cbac685647b57ffccb77d630a5f0
Dominik Kotarski <dojitza@gmail.com>
2020-11-09 23:44:30 +0100

    Implemented an asynchronous polling mechanism for ispovest generation

--- src/IspovestGenerationModal.js
+++ src/IspovestGenerationModal.js
@@ -10,67 +10,88 @@ export function IspovestGenerationModal(props) {
   const [showIntro, setShowIntro] = useState(true);
   const [generatedIspovest, setGeneratedIspovest] = useState({
     id: null,
-    text: "Ovde će da se prikaže generisovana ispovest",
+    text: null,
   });
-  const [queuePosition, setQueuePosition] = useState(-1);
+  const [generationState, setGenerationState] = useState(
+    constants.GENERATION_STATE_INITIAL
+  );
+  const [queuePosition, setQueuePosition] = useState();
   const [prefix, setPrefix] = useState("");
   const [authorName, setAuthorName] = useState("");
-  const [waitingForGeneration, setWaitingForGeneration] = useState(false);
   const [waitingForPublish, setWaitingForPublish] = useState(false);
-  const [eta, setEta] = useState(-1);
+  const [eta, setEta] = useState(constants.GENERATION_ESTIMATE_SECONDS);
+  const waitingForGeneration =
+    generationState === constants.GENERATION_STATE_GENERATING;
 
   useEffect(() => {
-    const tick = () => {
-      setEta(eta - 1);
-      if (eta % constants.GENERATION_ESTIMATE_SECONDS == 1) {
-        queuePosition > 0 && setQueuePosition(queuePosition - 1);
-      }
-      if (eta <= 1) {
-        setEta(9);
-      }
-    };
     const intervalRef = setInterval(() => {
       tick();
     }, 1000);
     return () => clearInterval(intervalRef);
   });
 
-  const getQueuePosition = async () => {
-    const response = await fetch(`${constants.API_ROOT}/queueLength`);
-    const queuePosition = await response.json();
-    setQueuePosition(parseInt(queuePosition));
-    setEta(
-      (parseInt(queuePosition) + 1) * constants.GENERATION_ESTIMATE_SECONDS
-    );
+  const tick = () => {
+    if (waitingForGeneration) {
+      setEta(eta - 1);
+      if (
+        generationState === constants.GENERATION_STATE_GENERATING &&
+        (eta <= 1 || eta % 10 === 0)
+      ) {
+        pollForGeneratedIspovest();
+      }
+      if (eta <= 1) {
+        setEta(9);
+      }
+    }
   };
 
   const sendGenerateRequest = async (prefix) => {
-    setWaitingForGeneration(true);
-
     setGeneratedIspovest({ id: null, text: null });
-    await getQueuePosition();
+    const response = await fetch(`${constants.API_ROOT}/generateIspovest`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prefix: prefix }),
+    });
+    if (!response.isError) {
+      setGenerationState(constants.GENERATION_STATE_GENERATING);
+      pollForGeneratedIspovest();
+    }
+  };
 
-    const response = await fetch(
-      `${constants.API_ROOT}/generateIspovest?prefix=${prefix}`
-    );
-    const ispovest = await response.json();
-    setGeneratedIspovest(ispovest);
-
-    setWaitingForGeneration(false);
+  const pollForGeneratedIspovest = async () => {
+    const response = await fetch(`${constants.API_ROOT}/generateIspovest`);
+    if (!response.isError) {
+      const responseJson = await response.json();
+      console.log(responseJson);
+      if (responseJson.id) {
+        setGeneratedIspovest(responseJson);
+        setGenerationState(constants.GENERATION_STATE_GENERATED);
+      } else {
+        if (responseJson.queuePosition != queuePosition) {
+          setEta(
+            (responseJson.queuePosition + 1) *
+              constants.GENERATION_ESTIMATE_SECONDS
+          );
+          setQueuePosition(responseJson.queuePosition);
+        }
+      }
+    }
   };
 
   const publishIspovest = async (id, authorName) => {
     setWaitingForPublish(true);
-    const response = await fetch(
-      `${constants.API_ROOT}/publishIspovest?ispovestId=${id}&authorName=${authorName}`,
-      {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(""),
-      }
-    );
+    const response = await fetch(`${constants.API_ROOT}/publishIspovest`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ispovestId: id,
+        authorName: authorName,
+      }),
+    });
     if (response.ok) {
       setShowGenerationModal(false);
       updateData();
@@ -78,6 +99,23 @@ export function IspovestGenerationModal(props) {
       alert("Došlo je do greške, pokušajte opet opet");
     }
     setWaitingForPublish(false);
+  };
+
+  const renderIspovestDivText = (generationState) => {
+    switch (generationState) {
+      case constants.GENERATION_STATE_INITIAL:
+        return "Ovde će da se prikaže generisovana ispovest";
+      case constants.GENERATION_STATE_GENERATING:
+        return (
+          <Spinner style={{ padding: 30 }} animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        );
+      case constants.GENERATION_STATE_GENERATED:
+        return generatedIspovest.text;
+      default:
+        return "Ovde će da se prikaže generisovana ispovest";
+    }
   };
 
   return (
@@ -143,17 +181,7 @@ export function IspovestGenerationModal(props) {
               </button>
             </Row>
             <Row className="modalRow ispovestText generatedIspovestText">
-              {waitingForGeneration ? (
-                <Spinner
-                  style={{ padding: 30 }}
-                  animation="border"
-                  role="status"
-                >
-                  <span className="sr-only">Loading...</span>
-                </Spinner>
-              ) : (
-                generatedIspovest.text
-              )}
+              {renderIspovestDivText(generationState)}
             </Row>
             {queuePosition >= 0 && !generatedIspovest.id && (
               <Row className="modalRow queueInfoText">
