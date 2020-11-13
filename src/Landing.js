9c244e0fff931e0e9ed39e0f4d08101c64266fd7
Dominik Kotarski <dojitza@gmail.com>
2020-11-13 23:22:04 +0100

    Updated landing, changed generation estimate, submission throttle to 5 minutes

--- src/Landing.js
+++ src/Landing.js
@@ -8,6 +8,18 @@ export function Landing() {
     <Container className="landingContainer">
       <Col>
         <Row className="landingRow">
+          <Col className="landingCol">
+            <Link to="/generated" className="landingButton generiraj">
+              Generiraj nove i sudi
+            </Link>
+          </Col>
+          <Col className="landingCol">
+            <Link to="/published" className="landingButton citaj">
+              Čitaj stare
+            </Link>
+          </Col>
+        </Row>
+        <Row className="landingRow">
           <Col className="landingInfo">
             Dobrodošli na ispovesti.ml. Na ovom sajtu pronaći ćete ispovesti
             generirane generativnim tekstualnim modelom pod imenom{" "}
@@ -18,19 +30,7 @@ export function Landing() {
             treniranja modela kako bi upio njihovu bit. Rezultirajući model
             generirao je sve ispovesti koje se nalaze na ovom sajtu. Više o
             projektu možete pročitati na sledećem{" "}
-            <a href="https://dojitza.github.io/projects/ispovestiml/">linku</a>
-          </Col>
-        </Row>
-        <Row className="landingRow">
-          <Col className="landingCol">
-            <Link to="/generated" className="landingButton generiraj">
-              Generiraj nove i sudi
-            </Link>
-          </Col>
-          <Col className="landingCol">
-            <Link to="/published" className="landingButton citaj">
-              Čitaj stare
-            </Link>
+            <a href="https://dojitza.github.io/projects/Ispovesti.ml/">linku</a>
           </Col>
         </Row>
       </Col>
