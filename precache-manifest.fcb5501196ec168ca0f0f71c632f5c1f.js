7892515c96766e67607c1bbbc4c6fcf36a55c372
Dominik Kotarski <dojitza@gmail.com>
2021-04-28 20:54:56 +0200

    Updates

---
+++ precache-manifest.fcb5501196ec168ca0f0f71c632f5c1f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208433e3b5acd65aef1e44a819cff4a6",
+    "url": "/ispovesti-ml.frontend/index.html"
+  },
+  {
+    "revision": "e81d6f0c539b98847301",
+    "url": "/ispovesti-ml.frontend/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "a5390f4715467947d7df",
+    "url": "/ispovesti-ml.frontend/static/css/main.39dd2b1b.chunk.css"
+  },
+  {
+    "revision": "e81d6f0c539b98847301",
+    "url": "/ispovesti-ml.frontend/static/js/2.dffe9a9d.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/ispovesti-ml.frontend/static/js/2.dffe9a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5390f4715467947d7df",
+    "url": "/ispovesti-ml.frontend/static/js/main.11524fe9.chunk.js"
+  },
+  {
+    "revision": "90293b0857de324d6c65",
+    "url": "/ispovesti-ml.frontend/static/js/runtime-main.81ea85f4.js"
+  },
+  {
+    "revision": "bbfb9dd4a5842bb3ec56b992dc19cd55",
+    "url": "/ispovesti-ml.frontend/static/media/bluembo.bbfb9dd4.jpg"
+  },
+  {
+    "revision": "dddde1cf2eb7f7771e843f631a01e67c",
+    "url": "/ispovesti-ml.frontend/static/media/circuit-texture.dddde1cf.jpg"
+  }
+]);
